eec62d6e54626dc3ed11797564e654ac014ea244
Rafael Machado <machadorafael27@gmail.com>
2023-11-28 13:45:20 -0300

    exercise 6 check

--- src/pages/ProductsList/ProductsList.tsx
+++ src/pages/ProductsList/ProductsList.tsx
@@ -1,27 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import { getCategories } from '../../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../../services/api';
 
 type Category = {
-  id: number;
+  id: string;
   name: string;
 };
-
+type Product = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  price: number;
+};
 function ProductsList() {
   const [categories, setCategories] = useState<Category[]>([]);
-
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>('');
   useEffect(() => {
-    getCategories().then(setCategories);
+    getCategories().then((categoriesData) => {
+      setCategories(categoriesData);
+    });
   }, []);
-
+  useEffect(() => {
+    if (selectedCategory !== null) {
+      getProductsFromCategoryAndQuery(selectedCategory, searchQuery)
+        .then((productsData) => {
+          setProducts(productsData.results);
+        });
+    }
+  }, [selectedCategory, searchQuery]);
+  const handleCategoryClick = (categoryId: string) => {
+    setSelectedCategory(categoryId);
+  };
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
   return (
     <>
       <h2>Categorias</h2>
       <ul>
         {categories.map((category: Category) => (
-          <li key={ category.id } data-testid="category">{category.name}</li>))}
+          // <li key={ category.id } onClick={ () => handleCategoryClick(category.id) } data-testid="category">
+          <li key={ category.id }>
+            <input
+              type="button"
+              value={ category.name }
+              key={ category.id }
+              onClick={ () => handleCategoryClick(category.id) }
+              data-testid="category"
+            />
+          </li>
+
+        ))}
       </ul>
+
+      {products.length > 0 && (
+        <>
+          <h2>Produtos da Categoria</h2>
+          <ul>
+            {products.map((product: Product) => (
+              <li key={ product.id } data-testid="product">
+                <img src={ product.thumbnail } alt={ product.title } />
+                <div>{product.title}</div>
+                <div>{product.price}</div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
-
 export default ProductsList;
