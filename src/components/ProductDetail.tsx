fefb0dcb5a4ff6085571b0a44cdefb76beacb887
Gabriel Moreira <gabrielms.cec@gmail.com>
2023-11-29 11:40:41 -0300

    requisitos-08-e-09

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,15 +1,31 @@
 import { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-function ProductDetail() {
-  const { productId } = useParams<{ productId: string }>();
-  const [product, setProduct] = useState<any | null>(null);
+type Product = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  price: number;
+};
+
+interface ProductsListProps {
+  addToCart: (
+    productId: string,
+    productName: string,
+    productThumbnail: string,
+    productPrice: number
+  ) => void;
+}
+
+function ProductDetail({ addToCart }: ProductsListProps) {
+  const { productId: routeProductId } = useParams<{ productId: string }>();
+  const [product, setProduct] = useState<Product>();
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchProductDetails = async () => {
       try {
-        const apiUrl = `https://api.mercadolibre.com/items/${productId}`;
+        const apiUrl = `https://api.mercadolibre.com/items/${routeProductId}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
@@ -23,7 +39,7 @@ function ProductDetail() {
     };
 
     fetchProductDetails();
-  }, [productId]);
+  }, [routeProductId]);
 
   if (loading) {
     return <p>Carregando...</p>; // mensagem de carregamento enquanto os detalhes do produto estão sendo buscados
@@ -33,29 +49,41 @@ function ProductDetail() {
     return <p>Não foi possível carregar os detalhes do produto.</p>; // mensagem de erro se não conseguir obter os detalhes do produto
   }
 
+  const handleClickAddToCart = (
+    productId: string,
+    productName: string,
+    productImage: string,
+    productPrice: number,
+  ) => {
+    addToCart(productId, productName, productImage, productPrice);
+  };
+
   return (
-    <div>
-      <h2 data-testid="product-detail-name">{product.title}</h2>
-      <img
-        src={ product.thumbnail }
-        alt={ product.title }
-        data-testid="product-detail-image"
-      />
-      <p data-testid="product-detail-price">
-        Preço: R$
-        {product.price}
-      </p>
-
-      {/* Se for preciso adicionar outras infos para o produto, pode inseri-las aqui */}
-
-      {/* Botão para ir para o carrinho, quando for implementado essa função futuramente */}
-      {/* <Link
-        to="/shoppingcart"
-        data-testid="shopping-cart-button"
+    <>
+      <div>
+        <h2 data-testid="product-detail-name">{product.title}</h2>
+        <img
+          src={ product.thumbnail }
+          alt={ product.title }
+          data-testid="product-detail-image"
+        />
+        <p data-testid="product-detail-price">
+          Preço: R$
+          {product.price}
+        </p>
+      </div>
+      <button
+        onClick={ () => handleClickAddToCart(
+          product.id,
+          product.title,
+          product.thumbnail,
+          product.price,
+        ) }
+        data-testid="product-detail-add-to-cart"
       >
-        <button>Ir para o Carrinho</button>
-      </Link> */}
-    </div>
+        Adicionar ao Carrinho
+      </button>
+    </>
   );
 }
 export default ProductDetail;
